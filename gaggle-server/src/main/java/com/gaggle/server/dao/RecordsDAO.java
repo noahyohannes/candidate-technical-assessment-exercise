4a95fb6be4de7cc8a8c278f746798625b39f538b
Noah Yohannes <noahyohannes@Noahs-MacBook-Pro.local>
2021-08-28 11:46:45 -0400

    Removed un-used method from RecordsDAO interface.

--- gaggle-server/src/main/java/com/gaggle/server/dao/RecordsDAO.java
+++ gaggle-server/src/main/java/com/gaggle/server/dao/RecordsDAO.java
@@ -7,8 +7,6 @@ import com.gaggle.server.model.Contact;
 public interface RecordsDAO {
 
 	List<Contact> findAll();
-
-//	String getNameFromId(Long contactId);
 	
 	public Contact getContactById(Long contactId);
 
